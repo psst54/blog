9b33e58714b06ab786c2d3504b4cd7a7e5782b53
psst54 <psst5491@naver.com>
2023-08-10 15:36:12 +0900

    ⚡️ lazy load component

--- app/components/PostContent/index.tsx
+++ app/components/PostContent/index.tsx
@@ -1,4 +1,5 @@
-import ReactMarkdown from "react-markdown";
+import loadable from "@loadable/component";
+const ReactMarkdown = loadable(() => import("react-markdown"));
 import remarkGfm from "remark-gfm";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { nord } from "react-syntax-highlighter/dist/cjs/styles/prism";
