9c86502684b1c6a3c6cc27bedd29255f59e345d4
psst54 <psst5491@naver.com>
2023-08-10 10:57:44 +0900

    💄 style markdown

--- app/routes/subBlog.$postId.tsx
+++ app/routes/subBlog.$postId.tsx
@@ -1,9 +1,22 @@
 import react from "react";
 import type { LoaderArgs } from "@remix-run/cloudflare";
 import { useLoaderData } from "@remix-run/react";
+import ReactMarkdown from "react-markdown";
 
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "@supabase/types";
+
+import {
+  styledH1,
+  styledH2,
+  styledH3,
+  styledP,
+  styledA,
+  styledLi,
+  styledOl,
+  styledUl,
+  styledBlockquote,
+} from "@styles/markdown";
 
 export const loader = async ({ context, params }: LoaderArgs) => {
   const supabase = createClient<Database>(
@@ -40,5 +53,31 @@ export const loader = async ({ context, params }: LoaderArgs) => {
 export default function PostPage() {
   const content = useLoaderData<typeof loader>();
 
-  return <div>{content}</div>;
+  return (
+    <div>
+      <div>
+        <ReactMarkdown
+          components={{
+            h1: (props: any) => <h1 css={styledH1} {...props} />,
+            h2: (props: any) => <h2 css={styledH2} {...props} />,
+            h3: (props: any) => <h3 css={styledH3} {...props} />,
+            h4: (props: any) => <h4 css={styledH3} {...props} />,
+            h5: (props: any) => <h5 css={styledH3} {...props} />,
+            h6: (props: any) => <h6 css={styledH3} {...props} />,
+            p: (props: any) => <p css={styledP} {...props} />,
+            a: (props: any) => <a target="_blank" css={styledA} {...props} />,
+            li: (props: any) => <li css={styledLi} {...props} />,
+            ol: (props: any) => <ol css={styledOl} {...props} />,
+            ul: (props: any) => <ul css={styledUl} {...props} />,
+            blockquote: (props: any) => (
+              <blockquote css={styledBlockquote} {...props} />
+            ),
+            pre: (props: any) => <pre css={{ overflow: "auto" }} {...props} />,
+          }}
+        >
+          {content}
+        </ReactMarkdown>
+      </div>
+    </div>
+  );
 }
---
+++ app/styles/markdown.ts
@@ -0,0 +1,46 @@
+export const styledH1 = {
+  padding: "1rem 0 0.2rem 0",
+};
+export const styledH2 = {
+  padding: "1rem 0 0.2rem 0",
+};
+export const styledH3 = {
+  padding: "0.8rem 0 0.2rem 0",
+};
+export const styledP = {
+  padding: "0.3rem 0",
+  lineHeight: "1.4rem",
+};
+export const styledA = {
+  color: "blue",
+  textDecoration: "underline",
+  padding: "0.3rem 0",
+  lineHeight: "1.4rem",
+};
+export const styledCode = {
+  padding: "0.1rem 0.2rem",
+  margin: "0 0.2rem",
+  borderRadius: "0.3rem",
+  background: "#2f343f",
+
+  fontFamily: "Pretendard Variable",
+  fontWeight: 600,
+  color: "#fff",
+};
+export const styledLi = {
+  marginLeft: "1.5rem",
+  lineHeight: "1.4rem",
+  padding: "0.3rem 0",
+};
+export const styledOl = {
+  padding: "0.3rem 0",
+};
+export const styledUl = {
+  padding: "0.3rem 0",
+};
+export const styledBlockquote = {
+  borderLeft: "3px solid black",
+  margin: "0.5rem 0",
+  padding: "0 1rem",
+  lineHeight: "1.4rem",
+};
