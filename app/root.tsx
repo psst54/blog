b5e969e9bfaf5683801ef4fc7aa1327e5411f6a1
psst54 <psst5491@naver.com>
2023-08-11 09:55:32 +0900

    🔍 setting language

--- app/root.tsx
+++ app/root.tsx
@@ -13,7 +13,7 @@ export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="ko">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
