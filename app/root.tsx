c54550b1d9cad86ee1c9fa54c6326cd66e3c087b
psst54 <psst5491@naver.com>
2023-08-11 10:06:03 +0900

    🔍 add title and description

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { LinksFunction } from "@remix-run/cloudflare";
+import type { LinksFunction, V2_MetaFunction } from "@remix-run/cloudflare";
 import {
   Links,
   LiveReload,
@@ -10,6 +10,13 @@ import {
 import styles from "@styles/global.css";
 
 export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
+
+export const meta: V2_MetaFunction = () => {
+  return [
+    { title: "PSST54's log" },
+    { name: "description", content: "PSST54의 블로그" },
+  ];
+};
 
 export default function App() {
   return (
