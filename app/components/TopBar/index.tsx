9a662724de843c2c1fdba33f86ae12250606e7a7
psst54 <psst5491@naver.com>
2023-08-31 17:58:46 +0900

    💄 add home icon

--- app/components/TopBar/index.tsx
+++ app/components/TopBar/index.tsx
@@ -1,6 +1,7 @@
 import { Link } from "@remix-run/react";
 
 import CategoroyPopUp from "@components/CategoryPopUp";
+import HomeIcon from "@assets/HomeIcon";
 
 const breakpoints = [1200, 576];
 const mq = breakpoints.map((bp) => `@media (max-width: ${bp}px)`);
@@ -26,7 +27,10 @@ export default function TopBar() {
         },
       }}
     >
-      <Link to={`/`}>Home</Link>
+      <Link to={`/`}>
+        <HomeIcon size="1.5rem" color="#000" />
+      </Link>
+
       <CategoroyPopUp />
     </div>
   );
