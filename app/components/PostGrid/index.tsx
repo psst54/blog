8e2cd109eed811d1988b15f9a4658da41781118e
psst54 <psst5491@naver.com>
2023-11-03 08:35:35 +0900

    💄 auto-fill -> auto-fit

--- app/components/PostGrid/index.tsx
+++ app/components/PostGrid/index.tsx
@@ -2,13 +2,21 @@ import { Link } from "@remix-run/react";
 import PostCard from "./PostCard";
 import { POST_WIDTH } from "./styles";
 
+const breakpoints = [1200, 576];
+const mq = breakpoints.map((bp) => `@media (max-width: ${bp}px)`);
+
 export default function PostGrid({ posts }: { posts: any }) {
   return (
     <div
       css={{
         display: "grid",
-        gridTemplateColumns: `repeat(auto-fill, minmax(${POST_WIDTH}, 1fr))`,
+        gridTemplateColumns: `repeat(auto-fit, ${POST_WIDTH})`,
         gap: "1.5rem",
+
+        [mq[0]]: {
+          gridTemplateColumns: `repeat(auto-fill, minmax(${POST_WIDTH}, 1fr))`,
+        },
+        [mq[1]]: {},
       }}
     >
       {posts?.map((post, postIdx: number) => {
