fea534cf0396a8469501c051816ef82e33905993
psst54 <psst5491@naver.com>
2023-11-03 18:44:25 +0900

    ♻️ 카테고리 모달 리팩토링

--- app/components/CategoryPopUp/index.tsx
+++ app/components/CategoryPopUp/index.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { useParams } from "@remix-run/react";
 
 import { color } from "@styles/color";
 import CategoryList from "@components/CategoryList";
@@ -7,72 +6,81 @@ import CategoryList from "@components/CategoryList";
 import RightChevronIcon from "@assets/RightChevronIcon";
 import ListIcon from "@assets/ListIcon";
 
+const openButton = {
+  display: "flex",
+  alignItems: "center",
+  gap: "0.5rem",
+
+  background: "transparent",
+  border: "none",
+  outline: "none",
+  cursor: "pointer",
+};
+
+const closeButton = {
+  background: "none",
+  border: "none",
+  outline: "none",
+  cursor: "pointer",
+};
+
+const buttonDescriptionText = { fontSize: "1.25rem" };
+
+const CategorySlide = {
+  position: "fixed" as "fixed",
+  width: "90vw",
+  maxWidth: "400px",
+  height: "100dvh",
+  top: 0,
+  right: 0,
+
+  display: "flex",
+  flexDirection: "column" as "column",
+
+  background: color.background.standard,
+  borderWidth: "2px 0 2px 2px",
+  borderStyle: "solid",
+  borderColor: color.border.standard,
+  borderRadius: "1rem 0 0 1rem",
+  padding: "1.25rem",
+
+  zIndex: 10,
+};
+
+const titleWrapper = { display: "flex", alignItems: "center", gap: "0.5rem" };
+
 export default function CategoroyPopUp({ data, isPostOpen, toggleCategory }) {
   const [isOpen, setIsOpen] = useState(false);
-  const params = useParams();
+
+  function onOpen() {
+    setIsOpen(true);
+  }
+
+  function onClose() {
+    setIsOpen(false);
+  }
 
   return (
     <div>
       <button
         aria-label="toggle category menu"
-        css={{
-          display: "flex",
-          alignItems: "center",
-          gap: "0.5rem",
-
-          background: "transparent",
-          border: "none",
-          outline: "none",
-          cursor: "pointer",
-        }}
-        onClick={() => {
-          setIsOpen(true);
-        }}
+        css={openButton}
+        onClick={onOpen}
       >
         <ListIcon size="1.5rem" color="#000" />
       </button>
 
       {isOpen && (
-        <div
-          css={[
-            {
-              position: "fixed",
-              width: "90vw",
-              maxWidth: "400px",
-              height: "100dvh",
-              top: 0,
-              right: 0,
-
-              display: "flex",
-              flexDirection: "column",
-
-              background: color.background.standard,
-              borderWidth: "2px 0 2px 2px",
-              borderStyle: "solid",
-              borderColor: color.border.standard,
-              borderRadius: "1rem 0 0 1rem",
-              padding: "1.25rem",
-
-              zIndex: 10,
-            },
-          ]}
-        >
-          <div css={{ display: "flex", alignItems: "center", gap: "0.5rem" }}>
+        <div css={CategorySlide}>
+          <div css={titleWrapper}>
             <button
               aria-label="toggle category menu"
-              css={{
-                background: "none",
-                border: "none",
-                outline: "none",
-                cursor: "pointer",
-              }}
-              onClick={() => {
-                setIsOpen(false);
-              }}
+              css={closeButton}
+              onClick={onClose}
             >
               <RightChevronIcon size="1rem" color="#000" />
             </button>
-            <h2 css={{ fontSize: "1.25rem", fontWeight: 400 }}>접기</h2>
+            <h2 css={buttonDescriptionText}>접기</h2>
           </div>
 
           <CategoryList
