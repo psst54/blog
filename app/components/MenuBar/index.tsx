2036b4dcc6e316c60e3208db22b94dafbc671307
psst54 <psst5491@naver.com>
2023-11-03 18:29:49 +0900

    🎨 메뉴바 스타일 코드 변수화

--- app/components/MenuBar/index.tsx
+++ app/components/MenuBar/index.tsx
@@ -5,8 +5,6 @@ const HomeIcon = loadable(() => import("@assets/HomeIcon"));
 const CodeIcon = loadable(() => import("@assets/CodeIcon"));
 const BookBookmarkIcon = loadable(() => import("@assets/BookBookmarkIcon"));
 const HeartIcon = loadable(() => import("@assets/HeartIcon"));
-const CallChatIcon = loadable(() => import("@assets/CallChatIcon"));
-const SettingsIcon = loadable(() => import("@assets/SettingsIcon"));
 
 const breakpoints = [1200, 576];
 const mq = breakpoints.map((bp) => `@media (max-width: ${bp}px)`);
@@ -14,14 +12,40 @@ const mq = breakpoints.map((bp) => `@media (max-width: ${bp}px)`);
 const mainMenu = [
   { icon: HomeIcon, href: "/", alt: "Home" },
   { icon: CodeIcon, href: "/cse", alt: "CSE" },
-  // { icon: BookBookmarkIcon, href: "/phi", alt: "PHI" },
   { icon: HeartIcon, href: "/like", alt: "Like" },
 ];
 
-const bottomMenu = [
-  // { icon: CallChatIcon, href: "/contact", alt: "연락" },
-  // { icon: SettingsIcon, href: "/setting", alt: "설정" },
-];
+const MENU_WIDTH = "6rem";
+
+const container = {
+  flexShrink: 0,
+  width: MENU_WIDTH,
+  height: "100dvh",
+  paddingBottom: "1rem",
+
+  [mq[0]]: {
+    width: 0,
+  },
+};
+
+const innerContainer = {
+  position: "fixed",
+  display: "flex",
+  flexDirection: "column" as "column",
+  gap: "1rem",
+  alignItems: "center",
+
+  width: "MENU_WIDTH",
+  height: "100%",
+  padding: "1rem",
+
+  overflowY: "auto",
+
+  [mq[0]]: {
+    display: "none",
+    width: "1rem",
+  },
+};
 
 const iconList = {
   display: "flex",
@@ -31,39 +55,8 @@ const iconList = {
 
 export default function MenuBar() {
   return (
-    <div
-      css={{
-        flexShrink: 0,
-        width: "6rem",
-        height: "100dvh",
-        paddingBottom: "1rem",
-
-        [mq[0]]: {
-          width: 0,
-        },
-      }}
-    >
-      <div
-        css={{
-          position: "fixed",
-          display: "flex",
-          flexDirection: "column" as "column",
-          gap: "1rem",
-          alignItems: "center",
-          justifyContent: "space-between",
-
-          width: "6rem",
-          height: "100%",
-          padding: "1rem 0",
-
-          overflowY: "auto",
-
-          [mq[0]]: {
-            display: "none",
-            width: "1rem",
-          },
-        }}
-      >
+    <div css={container}>
+      <div css={innerContainer}>
         <div css={iconList}>
           {mainMenu.map((menuItem, menuItemIdx) => (
             <Link
@@ -86,19 +79,6 @@ export default function MenuBar() {
               }}
             >
               <menuItem.icon size="2.5rem" color={color.border.standard} />
-            </Link>
-          ))}
-        </div>
-
-        <div css={iconList}>
-          {bottomMenu.map((menuItem, menuItemIdx) => (
-            <Link
-              aria-label={menuItem.href}
-              key={menuItemIdx}
-              to={menuItem.href}
-              css={{ cursor: "pointer" }}
-            >
-              <menuItem.icon size="3.5rem" color={color.border.standard} />
             </Link>
           ))}
         </div>
