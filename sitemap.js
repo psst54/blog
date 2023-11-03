01515b04af0b9812b9f662ce66d26e48d7444398
psst54 <psst5491@naver.com>
2023-11-03 21:28:08 +0900

    🚧 sitemap 뼈대 추가

--- sitemap.js
+++ sitemap.js
@@ -1,5 +1,24 @@
 function main() {
-  console.log("hello world!!!");
+  const urls = ["https://blog.psst54.me/"];
+
+  const xmlHeader = '<?xml version="1.0" encoding="UTF-8"?>';
+  let xmlSitemap =
+    '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+
+  for (const url of urls) {
+    xmlSitemap += `
+    <url>
+      <loc>${url}</loc>
+      <lastmod>${new Date().toISOString()}</lastmod>
+      <changefreq>weekly</changefreq>
+      <priority>0.8</priority>
+    </url>
+  `;
+  }
+
+  xmlSitemap += "</urlset>";
+
+  console.log(xmlHeader + xmlSitemap);
 }
 
 main();
