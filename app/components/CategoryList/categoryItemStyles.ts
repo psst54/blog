2981620e06491b59648b300a6bc6e0c658bdd955
psst54 <psst5491@naver.com>
2024-03-29 22:12:32 +0900

    💄 change hover color

--- app/components/CategoryList/categoryItemStyles.ts
+++ app/components/CategoryList/categoryItemStyles.ts
@@ -12,20 +12,25 @@ export const ItemContainer = (indent: number) => {
 
     borderRadius: "2rem",
 
-    "&:hover": { background: COLOR.PRIMARY.SHADOW + "64" },
+    color: COLOR.TEXT.STANDARD,
+    fontWeight: 500,
+
+    "&:hover": {
+      background: COLOR.PRIMARY.SHADOW + "64",
+    },
   };
 };
 
 export const Title = {
   flexGrow: 1,
   marginLeft: "0.1rem",
-  fontSize: "1rem",
-  fontWeight: 400,
 
   overflow: "hidden",
   whiteSpace: "nowrap" as const,
   textOverflow: "ellipsis",
   wordBreak: "break-all" as const,
+
+  color: "inherit",
 };
 
 export const ToggleButton = {
--- app/constants/color.ts
+++ app/constants/color.ts
@@ -15,6 +15,6 @@ export const COLOR = {
     LINK: "#247bff",
   },
 
-  PRIMARY: { STANDARD: "#fd92d6", SHADOW: "#F2D2DD" },
+  PRIMARY: { STANDARD: "#fc81c7", SHADOW: "#F2D2DD", DARK: "#f76ab8" },
   SECONDARY: { STANDARD: "#7eaef7" },
 };
