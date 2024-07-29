e40d15e3eaa9932464581c4cc3a98ad6ae7c501b
psst54 <psst5491@naver.com>
2024-07-29 11:29:50 +0900

    🚧 getPostList function

--- app/utils/supabase/getPostList.ts
+++ app/utils/supabase/getPostList.ts
@@ -3,25 +3,19 @@ import type { Database } from "@supabase/types";
 import { NORMAL_PAGE, type Post } from "~/types";
 import { POST_SUMMARY_ATTR, POST_TABLE } from ".";
 
-export async function getRecentPostList({
+export async function getPostList({
   supabaseClient,
-  subBlogId = "cse",
-  count = 10,
   showAll = true,
 }: {
   supabaseClient: SupabaseClient<Database, "public">;
-  subBlogId?: string;
-  count?: number;
   showAll?: boolean;
 }): Promise<Post[]> {
   const { data, error } = await supabaseClient
     .from(POST_TABLE)
     .select(POST_SUMMARY_ATTR)
     .in("show_main", showAll ? [true, false] : [true])
-    // .eq("sub_blog", subBlogId)
     .eq("type", NORMAL_PAGE)
     .order("created_at", { ascending: false })
-    .limit(count)
     .returns<Post[]>();
 
   if (error) return [];
