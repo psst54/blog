6ade6dd619012c0a6b4e16e49bb2522c25cbe106
Dahye <psst5491@naver.com>
2024-12-23 21:13:56 +0900

    ✨ add category to main page

--- app/routes/_index/index.tsx
+++ app/routes/_index/index.tsx
@@ -1,9 +1,12 @@
 import { useLoaderData } from "@remix-run/react";
-import type { Post, SupabaseKey } from "~/types";
+import type { Category, Post, SupabaseKey } from "~/types";
 
 import NavBar from "@components/NavBar";
 import Page from "@components/Page";
 import Content from "./components/Content";
+import { useEffect, useState } from "react";
+import fetchCategoryData from "../$subBlogId/fetchCategoryData";
+import toggleCategory from "../$subBlogId/toggleCategory";
 
 export { loader } from "./utils/loader";
 export { meta } from "./utils/meta";
@@ -19,9 +22,34 @@ export default function Index() {
     supabaseKey: SupabaseKey;
   } = useLoaderData();
 
+  const [categoryData, setCategoryData] = useState<Category[]>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await fetchCategoryData(
+        "cse",
+        supabaseKey.SUPABASE_URL,
+        supabaseKey.SUPABASE_KEY
+      );
+      setCategoryData(data);
+    }
+
+    fetchData();
+  }, [supabaseKey]);
+
+  const onToggleCategory = (id: string) => {
+    setCategoryData(toggleCategory(id, categoryData));
+  };
+
   return (
     <Page
-      navbar={<NavBar supabaseKey={supabaseKey} />}
+      navbar={
+        <NavBar
+          data={categoryData}
+          onToggleCategory={onToggleCategory}
+          supabaseKey={supabaseKey}
+        />
+      }
       body={
         <Content
           recentPostList={recentPostList}
