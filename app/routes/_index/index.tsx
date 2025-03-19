1f25fea10b63849f9a3713d4af7ab1bd419c96b0
Dahye <psst5491@naver.com>
2025-03-19 21:21:22 +0900

    ✨ 메인페이지 tanstack query 적용

--- app/routes/_index/index.tsx
+++ app/routes/_index/index.tsx
@@ -1,65 +1,28 @@
-import { useEffect, useMemo, useState } from "react";
-import { useOutletContext } from "@remix-run/react";
-import { createClient } from "@supabase/supabase-js";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import type { Document } from "~/types/post";
 import Page from "~/components/Page";
 
-import { getPinnedPostList, getRecentPostList } from "./_utils";
 import { PinnedSection, RecentSection } from "./_components";
 
 export { meta } from "./_utils/meta";
 
-interface SupabaseCredential {
-  url: string;
-  key: string;
-}
-
-interface OutletContextType {
-  supabaseCredential?: SupabaseCredential;
-}
+const queryClient = new QueryClient();
 
 export default function Index() {
-  const { supabaseCredential } = useOutletContext<OutletContextType>();
-  const [recentPostList, setRecentPostList] = useState<Document[]>([]);
-  const [pinnedPostList, setPinnedPostList] = useState<Document[]>([]);
-
-  const supabaseClient = useMemo(() => {
-    if (!supabaseCredential) {
-      return null;
-    }
-
-    return createClient(supabaseCredential.url, supabaseCredential.key);
-  }, [supabaseCredential]);
-
-  useEffect(() => {
-    if (!supabaseClient) {
-      return;
-    }
-
-    (async () => {
-      const [recentPosts, pinnedPosts] = await Promise.all([
-        getRecentPostList({ supabaseClient, showAll: false }),
-        getPinnedPostList({ supabaseClient }),
-      ]);
-
-      setRecentPostList(recentPosts);
-      setPinnedPostList(pinnedPosts);
-    })();
-  }, [supabaseClient]);
-
   return (
-    <Page>
-      <div
-        css={{
-          display: "flex",
-          flexDirection: "column",
-          gap: "2rem",
-        }}
-      >
-        <PinnedSection postList={pinnedPostList} />
-        <RecentSection postList={recentPostList} />
-      </div>
-    </Page>
+    <QueryClientProvider client={queryClient}>
+      <Page>
+        <div
+          css={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "2rem",
+          }}
+        >
+          <PinnedSection />
+          <RecentSection />
+        </div>
+      </Page>
+    </QueryClientProvider>
   );
 }
