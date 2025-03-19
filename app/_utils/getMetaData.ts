d3b85f3a6edbba2487d1bb68336b823bf83c69a9
Dahye <psst5491@naver.com>
2025-03-19 18:42:50 +0900

    🔍 meta tag 수정

--- app/_utils/getMetaData.ts
+++ app/_utils/getMetaData.ts
@@ -3,6 +3,7 @@ const TITLE = "프론트엔드 개발자의 기술 블로그";
 const AUTHOR = "psst54";
 const DESCRIPTION =
   "개발하면서 배운 것들을 정리합니다 | 프로젝트 회고와 트러블 슈팅 | JS/TS, React, Git";
+const DEFAULT_IMAGE = "https://psst54.me/default-thumbnail.jpg"; // 기본 썸네일
 
 export default function getMetaData({
   pathname,
@@ -23,7 +24,9 @@ export default function getMetaData({
   data.push({ name: "og:site_name", content: SITE_NAME });
   data.push({ name: "og:type", content: "website" });
 
-  const titleText = `${SITE_NAME} | ${title || TITLE}`;
+  const titleText = title
+    ? `${title} | ${SITE_NAME}`
+    : `${SITE_NAME} | ${TITLE}`;
   data.push({ title: titleText });
   data.push({ name: "og:title", content: titleText });
 
@@ -31,19 +34,21 @@ export default function getMetaData({
   data.push({ name: "description", content: subTitleText });
   data.push({ name: "og:description", content: subTitleText });
 
-  const tagText = tagList || "코딩";
+  const tagText = tagList || "프론트엔드, 개발, JavaScript, React, Git";
   data.push({ name: "keywords", content: tagText });
 
-  if (thumbnail) {
-    data.push({ name: "og:image", content: thumbnail });
-    data.push({ name: "twitter:image", content: thumbnail });
-  }
+  const ogImage = thumbnail || DEFAULT_IMAGE;
+  data.push({ name: "og:image", content: ogImage });
+  data.push({ name: "og:image:width", content: "1200" });
+  data.push({ name: "og:image:height", content: "630" });
 
+  const pageUrl = `https://psst54.me${pathname}`;
   data.push({
     tagName: "link",
     rel: "canonical",
-    href: `https://psst54.me${pathname}`,
+    href: pageUrl,
   });
+  data.push({ name: "og:url", content: pageUrl });
 
   data.push({ name: "robots", content: "index, follow" });
   data.push({ name: "NaverBot", content: "All" });
@@ -54,6 +59,8 @@ export default function getMetaData({
   data.push({ name: "twitter:card", content: "summary_large_image" });
   data.push({ name: "twitter:title", content: titleText });
   data.push({ name: "twitter:description", content: subTitleText });
+  data.push({ name: "twitter:image", content: ogImage });
+  data.push({ name: "twitter:site", content: "@Panta_rH3i" });
 
   return data;
 }
